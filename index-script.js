4c4d403248ea85042d5e14a94dbdc1030b14315e
VanessaMP <vanessa.MP@outlook.fr>
2021-08-07 11:49:03 +0200

    Debut du projet ... travaux en cours...

---
+++ index-script.js
@@ -0,0 +1,15 @@
+//Recuperer les donnees JSON avec la methode fetch() (creer une requête fetch)
+
+fetch('./data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        console.log(json);
+    })
+    .catch(function() {
+        this.dataError = true;
+    })
