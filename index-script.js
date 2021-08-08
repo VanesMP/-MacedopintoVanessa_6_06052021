28310ef00e6a5d5996531e53ffccea78ada373b9
VanesMP <vanessa.MP@outlook.fr>
2021-08-08 13:51:23 +0200

    essai recuperation donnees

--- index-script.js
+++ index-script.js
@@ -13,3 +13,12 @@ fetch('fisheyeData.json')
     .catch(function() {
         this.dataError = true;
     })
+
+//Ajouter les données JSON à la page, 1: recuperer l' élément section. 2:parcourir chaque objet de notre objet JSON. 3: ajouter chaque objet à sa propre div dans le Html
+function appendData(photographer) {
+    var mainContainer = document.getElementById("pattern-photographers");
+    for (var i = 0; i < photographer.length; i++) {
+        var div = document.createElement('div').textContent = photographer[i].name;
+        mainContainer.appendChild(div);
+    }
+}
