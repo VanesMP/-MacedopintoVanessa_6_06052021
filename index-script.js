97c1f6970b65e233f879eeaf7b722075a3c2f2b8
VanesMP <vanessa.MP@outlook.fr>
2021-08-08 13:01:35 +0200

    debut projet

--- index-script.js
+++ index-script.js
@@ -1,6 +1,6 @@
 //Recuperer les donnees JSON avec la methode fetch() (creer une requête fetch)
 
-fetch('./data.json')
+fetch('data.json')
     .then(response => {
         if (!response.ok) {
             throw new Error("HTTP error " + response.status);
