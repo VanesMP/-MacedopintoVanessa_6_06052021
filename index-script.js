11786625fc6597c72a0b6f93cb329b601985e6f9
VanesMP <vanessa.MP@outlook.fr>
2021-08-08 17:58:28 +0200

    modif

--- index-script.js
+++ index-script.js
@@ -5,7 +5,7 @@ fetch('fisheyeData.json')
         return response.json();
     })
     .then(json => {
-        console.log(json.gestionPhotographer(photographers));
+        return json.gestionPhotographer(photographers);
     })
     .catch(function() {
 
